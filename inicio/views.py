8fc371ea5a49d3b8e08e8113475b9a9d3a7c51a8
BryanLarico <blaricor@unsa.edu.pe>
2024-06-19 14:40:34 -0500

    Subiendo la parte del trabajo de Django3 para poder ver los cambios realizados en los posteriores commits

---
+++ inicio/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+
+# Create your views here.
+
+def myHomeview(request, *args, **kwargs):
+    myContext = {
+        'myText': 'Esto es sobre mi',
+        'myNumber': 123,
+        'myList': [22,33,44,55,66],
+    }
+    return render(request, 'home.html', myContext)
+  
+def anotherView(request, *args, **kwargs):
+    print(args, kwargs)
+    print(request.user)
+    return HttpResponse('<h1>Otra vista desde DJango</h1>')
