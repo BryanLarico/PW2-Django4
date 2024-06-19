01e3c85bb1dbf91e296330ce2c135320e6a1e2cc
BryanLarico <blaricor@unsa.edu.pe>
2024-06-19 15:15:34 -0500

    Widgets: implementando un widget en forms.py

--- personas/forms.py
+++ personas/forms.py
@@ -2,7 +2,16 @@ from django import forms
 from .models import Persona
 
 class RawPersonaForm(forms.Form):
-    nombre = forms.CharField(label = 'Your name')
+    nombre = forms.CharField(
+        widget = forms.Textarea(
+            attrs = {
+                'placeholder': 'Solo tu nombre, por favor',
+                'id': 'nombreID',
+                'class': 'special',
+                'cols': '10',
+                }
+            )
+        )
     apellidos = forms.CharField()
     edad = forms.IntegerField(initial = 20)
     donador = forms.BooleanField()
