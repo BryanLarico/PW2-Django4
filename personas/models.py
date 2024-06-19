41744538474d9bc5c5a19c7b28c4dda82bc7743c
BryanLarico <blaricor@unsa.edu.pe>
2024-06-19 16:19:34 -0500

    Enlaces dinamicos

--- personas/models.py
+++ personas/models.py
@@ -4,6 +4,9 @@ class Persona(models.Model):
     nombre = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
     edad = models.IntegerField(blank = True)
-    donador = models.BooleanField()
+    donador = models.BooleanField(default= False)
+    
+    def get_absolute_url(self):
+        return "/personas/" + str(self.id) + "/"
 
 # Create your models here.
