24c6b1f1b0d1973ffd7f586e3382fe0efe4750c4
BryanLarico <blaricor@unsa.edu.pe>
2024-06-19 15:11:34 -0500

    Campos en el formulario: Restricciones a los campos

--- personas/forms.py
+++ personas/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Persona
 
 class RawPersonaForm(forms.Form):
-    nombre = forms.CharField()
+    nombre = forms.CharField(label = 'Your name')
     apellidos = forms.CharField()
-    edad = forms.IntegerField()
+    edad = forms.IntegerField(initial = 20)
     donador = forms.BooleanField()
