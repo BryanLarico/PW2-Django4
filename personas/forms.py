5359932de47949cfa0cf5f059c0aa6e21f609373
BryanLarico <blaricor@unsa.edu.pe>
2024-06-19 15:50:34 -0500

    Validación extra de campos: usando clean_<fieldname>(), en este caso para que la primera letra de nombre sea en mayuscula

--- personas/forms.py
+++ personas/forms.py
@@ -1,17 +1,19 @@
 from django import forms
 from .models import Persona
 
-class RawPersonaForm(forms.Form):
-    nombre = forms.CharField(
-        widget = forms.Textarea(
-            attrs = {
-                'placeholder': 'Solo tu nombre, por favor',
-                'id': 'nombreID',
-                'class': 'special',
-                'cols': '10',
-                }
-            )
-        )
-    apellidos = forms.CharField()
-    edad = forms.IntegerField(initial = 20)
-    donador = forms.BooleanField()
+class PersonaForm(forms.ModelForm):
+    class Meta:
+        model = Persona
+        fields = [
+            'nombre',
+            'apellidos',
+            'edad',
+            'donador',
+            ]
+    def clean_nombre(self, *args, **kwargs):
+        print('paso')
+        name = self.cleaned_data.get('nombre')
+        if name.istitle():
+            return name;
+        else:
+            raise forms.ValidationError('Primera letra en Mayúscula')
